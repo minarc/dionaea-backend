19b691ce88ea68a88b03135affb761154f979c96
ranghild <arfrumo@gmail.com>
2019-12-05 18:55:15 +0900

    UPDATE :: allowed host

--- dionaea_backend/settings.py
+++ dionaea_backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '[::1]']
 
 # Application definition
 
